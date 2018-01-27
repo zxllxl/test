3d0d5eb66c6872ee82c690f67738e75aa239f855
zxllxl <463969482@qq.com>
2018-01-27 22:16:56 +0800

    npm

---
+++ cube.js
@@ -0,0 +1,30 @@
+var THREE = require('three');
+			
+// Creating the scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth/2, window.innerHeight/2, false );
+document.body.appendChild( renderer.domElement );
+
+// To create a cube
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+// Rendering the scene
+function animate() {
+	requestAnimationFrame( animate );
+	
+	// Animating the cube
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
+	// cube.rotation.z += 0.1;
+	
+	renderer.render( scene, camera );
+}
+animate();
